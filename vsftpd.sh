8217783ec568fa5df641693dd6ececbeca680861
prapdm <dmochowski@avenus.pl>
2017-02-08 17:28:03 +0100

    first commit

--- vsftpd.sh
+++ vsftpd.sh
@@ -4,6 +4,7 @@ echo "pasv_max_port=$PASV_MAX" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_min_port=$PASV_MIN" >> /etc/vsftpd/vsftpd.conf 
 echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftpd/vsftpd.conf 
 mkdir /home/vsftpd/${FTP_USER}/
+chown -R ftp:ftp /home/vsftpd/${FTP_USER}/
 echo "" >> /conf/vsftpd/users_config/${FTP_USER}
 echo "${FTP_USER}:$(openssl passwd -1 ${FTP_PASS})" >> /conf/vsftpd/virtual_users
 chmod 600 /conf/vsftpd/virtual_users
